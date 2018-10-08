150ca52590f54d6f8be19b50705a1bab7b07d29a
nsikaktom <nsikakthompson73@gmail.com>
2018-10-08 14:22:23 +0100

    Changed balance and deposit  amount

--- ATM.java
+++ ATM.java
@@ -1,27 +1,34 @@
-public class ATM{
+
+public class ATM {
 
     public double balance = 2000.0;
 
     //constructor
-    public ATM(){
+    public ATM() {
         System.out.println("Thank you for choosing to Bank with us");
     }
 
     /**
      * Check and Print balance
      */
-    public void checkBalance(){
+    public void checkBalance() {
         System.out.println("Your balance is :" + balance);
     }
 
-    
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println("Your just deposited: " + amount);
+        checkBalance(); // Check new balance
+    }
+
     public static void main(String[] args) {
-        
+
         /**
          * @param a -> object
          * new ATM() -> constructor
          */
-        ATM a  = new ATM(); 
+        ATM a = new ATM();
         a.checkBalance(); // call check balance
+        a.deposit(3000); // deposit 3000
     }
 }
